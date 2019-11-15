aa5314469b9fd8f492d1c9982850e0cc830ea8b9
Vincent Mesquita <vincent@go-aos.io>
2019-11-15 19:59:01 +0100

    feat(api): connect to mongo db

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,14 +1,20 @@
+import * as mongoose from 'mongoose'
+
 import {
   ApolloServer,
   mergeSchemas
 } from 'apollo-server'
 import authSchema from './services/auth/graphQL'
 
-const server = new ApolloServer({
-  playground: true,
-  schema: mergeSchemas({ schemas: [authSchema] })
-})
+mongoose.connect('mongodb://localhost:27017/EpiDraw', { useNewUrlParser: true })
+  .then(async () => {
+    const server = new ApolloServer({
+      playground: true,
+      schema: mergeSchemas({ schemas: [authSchema] })
+    })
 
-server.listen(4242)
-  .then(({ url }) => console.log(`🚀  Server ready at ${url}`))
-  .catch(err => console.log(err))
+    server.listen(4242)
+      .then(({ url }) => console.log(`🚀  Server ready at ${url}`))
+      .catch(err => console.log(err))
+  })
+  .catch(error => console.log(error))
