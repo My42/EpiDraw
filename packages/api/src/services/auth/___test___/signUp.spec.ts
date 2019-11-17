d42a83a469555a6e24155c74a8f6b99f6bd4c4cb
Vincent Mesquita <vincent@go-aos.io>
2019-11-17 14:45:30 +0100

    test(api): add "test" script

---
+++ packages/api/src/services/auth/___test___/signUp.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import { describe, test } from 'mocha'
+
+describe('api/services/auth/signUp', () => {
+  test('Y O L O', () => {
+    expect(1).to.be.equal(3)
+  })
+})
