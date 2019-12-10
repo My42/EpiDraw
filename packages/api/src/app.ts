d7336841bd57cb74e6254d077b1429d02d56727f
Mesquita Vincent <vincent.mesquita@epitech.eu>
2019-12-10 22:10:30 +0100

    Docker compose (#3)
    * chore(*): add docker-compose and server dockerfile with mongo db
    
    * chore(*): docker compose
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config
    
    * chore(*): circleCI config

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -6,14 +6,13 @@ import {
 } from 'apollo-server'
 import authSchema from './services/auth/graphQL'
 
-mongoose.connect('mongodb://localhost:27017/EpiDraw',
+mongoose.connect('mongodb://database:27017/EpiDraw',
   { useNewUrlParser: true, useUnifiedTopology: true })
   .then(async () => {
     const server = new ApolloServer({
       playground: true,
       schema: mergeSchemas({ schemas: [authSchema] })
     })
-
     server.listen(4242)
       .then(({ url }) => console.log(`🚀  Server ready at ${url}`))
       .catch(err => console.log(err))
--- packages/api/test/init.ts
+++ packages/api/test/init.ts
@@ -1,16 +1,22 @@
 import { after, before } from 'mocha'
 import * as mongoose from 'mongoose'
 
-console.log('INIT TESTS', before)
-
 before(async () => {
   console.log('BEFORE')
-  await mongoose.connect('mongodb://localhost:27017/EpiDrawTest',
-    { useNewUrlParser: true, useUnifiedTopology: true })
+  try {
+    await mongoose.connect('mongodb://database:27017/EpiDrawTest',
+      { useNewUrlParser: true, useUnifiedTopology: true })
+  } catch (e) {
+    console.log('ERROR = ', e)
+  }
 })
 
 after(async () => {
   console.log('AFTER')
-  await mongoose.connection.dropDatabase()
-  await mongoose.disconnect()
+  try {
+    await mongoose.connection.dropDatabase()
+    await mongoose.disconnect()
+  } catch (e) {
+    console.log('ERROR = ', e)
+  }
 })
