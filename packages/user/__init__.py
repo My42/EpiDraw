78f2dd66817c6023e2018636cf4649f595e7c617
Vincent Mesquita <vincent@go-aos.io>
2020-04-08 11:35:36 +0200

    chroe(user): add dockerfile

--- packages/user/__init__.py
+++ packages/user/__init__.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Vincent!'
+    return 'Hello, Paul!!!'
--- packages/user/scripts/dev.py
+++ packages/user/scripts/dev.py
@@ -3,4 +3,4 @@ import os
 os.environ['FLASK_ENV'] = 'development'
 os.environ['FLASK_APP'] = '__init__.py'
 
-os.system('flask run')
+os.system('flask run --host=0.0.0.0')
