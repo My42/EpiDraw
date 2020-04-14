cac5f018ba83b981504028da8686ef36ad4cf078
Vincent Mesquita <vincent@go-aos.io>
2020-04-14 21:31:27 +0200

    test(gateway): add tests

--- packages/gateway/src/schemas/auth/mutations/signUp/signUp.spec.ts
+++ packages/gateway/src/schemas/auth/mutations/signUp/signUp.spec.ts
@@ -1,8 +1,36 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import faker from 'faker';
+import sinon from 'sinon';
 
-describe('api/src/graphQL/Auth/mutations/signUp', () => {
-  it('should call signUp method of Auth service', async () => {
-    expect(1).to.be.equal(2);
+import { ApolloContext } from '@gateway/types/ApolloContext';
+import { User } from '@gateway/services/user';
+
+import signUp from './signUp';
+
+describe('[gateway]: schema/auth/mutation/signUp', () => {
+  const sandbox = sinon.createSandbox();
+
+  const createMockedContext = (): ApolloContext => ({
+    services: {
+      user: sandbox.createStubInstance<User>(User) as unknown as User,
+    },
+  });
+
+  it('should sign up an user', async () => {
+    const ctx = createMockedContext();
+    const input = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      username: faker.internet.userName(),
+    };
+
+    const result = await signUp({}, { input }, ctx);
+
+    const spy = ctx.services.user as unknown as sinon.SinonStubbedInstance<User>;
+
+    expect(spy.createOne.callCount).to.be.equal(1);
+    expect(spy.createOne.getCall(0).args[0]).to.be.deep.equal(input);
+    expect(result).to.be.a('boolean').to.be.equal(true);
   });
 });
