676a0f106f7e131892bb4419738566c2e567e678
Vincent Mesquita <vincent@go-aos.io>
2020-04-22 22:09:09 +0200

    chore(client): compatibility between next and materialUI

--- packages/client/pages/_document.tsx
+++ packages/client/pages/_document.tsx
@@ -1,11 +1,35 @@
+/* eslint-disable arrow-parens */
 import * as React from 'react';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheet = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => originalRenderPage({
+        enhanceApp: App => props => sheet.collect(
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <App {...props} />,
+        ),
+      });
+
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      ctx.renderPage(sheet);
+    }
   }
 
   render() {
